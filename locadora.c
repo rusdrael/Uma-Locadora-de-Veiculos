f1add3c893b0277177044422f9be2d36a9c15be4
rusdrael <85977202+rusdrael@users.noreply.github.com>
2021-10-23 01:59:22 -0300

    Create locadora.c

---
+++ locadora.c
@@ -0,0 +1,112 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///                 Centro de Ensino Superior do Seridó                     ///
+///               Departamento de Computação e Tecnologia                   ///
+///                  Disciplina DCT1106 -- Programação                      ///
+///                  Projeto Uma Locadora de Veículos                       ///
+///          Developed by @rusdrael and @matheusfaria21 - Out, 2021         ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+/////
+// Assinatura das funções
+void telaSobre(void);
+void telaPrincipal(void);
+void telaEquipe(void);
+
+/////
+// Programa principal
+int main(void) {
+    telaSobre();
+    telaPrincipal();
+    telaEquipe();
+    return 0;
+}
+
+/////
+// Funções
+
+void telaSobre(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Uma Locadora de Veículos                       ///\n");
+    printf("///            Developed by @rusdrael and @matheusfaria21 - Out, 2021       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Uma Locadora de Veículos = = = = =                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///  Programa para o projeto que será a avaliação do curso, produzido em    ///\n");
+    printf("///  linguagem C, durante a disciplina DCT1106 - Programação do curso de    ///\n");
+    printf("///  Bacharelado em Sistemas de Informação da Universidade Federal do Rio   ///\n");
+    printf("///  Grande do Norte. O programa contém os principais módulos e             ///\n");
+    printf("///  funcionalidades que serão exigidos na avaliação dos projetos. Todos    ///\n");
+    printf("///  os dados e informações utilizados neste programa são fictícios.        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaPrincipal(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Uma Locadora de Veículos                       ///\n");
+    printf("///             Developed by @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Uma Locadora de Veículos = = = = =                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Módulo Cliente                                            ///\n");
+    printf("///            2. Módulo Administrador                                      ///\n");
+    printf("///            3. Módulo Veículos Disponíveis                               ///\n");
+    printf("///            4. Módulo Veículos Alugados                                  ///\n");
+    printf("///            5. Módulo Relatórios                                         ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+
+void telaEquipe(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///             Universidade Federal do Rio Grande do Norte                 ///\n");
+    printf("///                 Centro de Ensino Superior do Seridó                     ///\n");
+    printf("///               Departamento de Computação e Tecnologia                   ///\n");
+    printf("///                  Disciplina DCT1106 -- Programação                      ///\n");
+    printf("///                  Projeto Uma Locadora de Veículos                       ///\n");
+    printf("///             Developed by @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///            = = = = = Uma Locadora de Veículos = = = = =                 ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Este projeto foi desenvolvido por:                           ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Rusdrael Antony de Araújo Freire                             ///\n");
+    printf("///            E-mail: antony.freire.086@ufrn.edu.br                        ///\n");
+    printf("///            Matheus Marcos de Medeiros Santos Faria                      ///\n");
+    printf("///            E-mail: matheus.faria.121@ufrn.edu.br                        ///\n");
+    printf("///            Redes sociais: @rusdrael e @matheusfaria1_                   ///\n");
+    printf("///    Repositório: https://github.com/rusdrael/Uma-Locadora-de-Veiculos    ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
