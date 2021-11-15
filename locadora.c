8fc7b99126d9625ed5b0db23b200a79cc2e6911a
Rusdrael <rusdrael1@hotmail.com>
2021-11-14 21:54:05 -0300

    Inserção do Módulo 2 (Administrador)

--- locadora.c
+++ locadora.c
@@ -374,10 +374,206 @@ void telaExcluirCliente(void) {
 }
 
 
-void moduloAdministrador(void) {
-	// modulo em desenvolvimento
+//////////////////////////
+// Módulo Administrador //
+//////////////////////////
 
+//Inspirado no modelo de @flgorgonio
+void moduloAdministrador(void) {
+    char escolha;
+    do {
+        escolha = telaMenuAdministrador();
+        switch(escolha) {
+            case '1': cadastrarAdministrador();
+                                break;
+            case '2': pesquisarAdministrador();
+                                break;
+            case '3': alterarAdministrador();
+                                break;
+            case '4': excluirAdministrador();
+                                break;
+        }
+    } while (escolha != '0');                                                      
 }
+
+
+void cadastrarAdministrador(void) {
+    telaCadastrarAdministrador();
+}
+
+
+void pesquisarAdministrador(void) {
+    telaPesquisarAdministrador();
+}
+
+void alterarAdministrador(void) {
+    telaAlterarAdministrador();
+}
+
+void excluirAdministrador(void) {
+    telaExcluirAdministrador();
+}
+
+
+char telaMenuAdministrador(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = = Menu Administrador  = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           1. Cadastrar um novo administrador                          ///\n");
+    printf("///           2. Pesquisar os dados de um administrador                   ///\n");
+    printf("///           3. Atualizar o cadastro de um administrador                 ///\n");
+    printf("///           4. Excluir um administrador do sistema                      ///\n");
+    printf("///           0. Voltar ao menu anterior                                  ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Escolha a opção desejada: ");                       
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    return op;
+}
+
+
+void telaCadastrarAdministrador(void) {
+    char cpf[12];
+    char nome[51];
+    char email[51];
+    char nasc[11];
+    char celular[12];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = Cadastrar Administrador = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           CPF (apenas números): ");
+    scanf("%[0-9]", cpf);
+    getchar();
+    printf("///           Nome completo: ");
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+    getchar();
+    printf("///           E-mail: ");
+    scanf("%[A-Za-z0-9@._]", email);
+    getchar();
+    printf("///           Data de Nascimento (dd/mm/aaaa):  ");
+    scanf("%[0-9/]", nasc);
+    getchar();
+    printf("///           Celular  (apenas números): ");
+    scanf("%[0-9]", celular);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaPesquisarAdministrador(void) {
+    char cpf[12];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = Pesquisar Administrador = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe o CPF (apenas números): ");
+    scanf("%[0-9]", cpf);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaAlterarAdministrador(void) {
+    char cpf[12];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = Alterar Administrador = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe o CPF (apenas números): ");
+    scanf("%[0-9]", cpf);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaExcluirAdministrador(void) {
+    char cpf[12];
+    
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = Excluir Administrador = = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe o CPF (apenas números): ");
+    scanf("%[0-9]", cpf);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+/////////////////////////////////
+// Módulo Veículos Disponíveis //
+/////////////////////////////////
 
 
 void moduloVeiculosDisp(void) {
@@ -385,10 +581,21 @@ void moduloVeiculosDisp(void) {
 
 }
 
+
+//////////////////////////////
+// Módulo Veículos Alugados //
+//////////////////////////////
+
+
 void moduloVeiculosAlug(void) {
 	// modulo em desenvolvimento
 
 }
+
+
+////////////////////////
+// Módulo Relatórios //
+///////////////////////
 
 
 void moduloRelatorios(void) {
