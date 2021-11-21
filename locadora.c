a676604dab5f3d84ac2a7be5181b296c1a9811f1
Rusdrael <rusdrael1@hotmail.com>
2021-11-21 03:08:27 -0300

    Inserção do Módulo 4 (Veículos Alugados)

--- locadora.c
+++ locadora.c
@@ -50,6 +50,15 @@ void atualizarVeiculosDisp(void);
 void excluirVeiculosDisp(void);
 
 void moduloVeiculosAlug(void);
+char telaMenuVeiculosAlug(void);
+void telaCadastrarVeiculosAlug(void);
+void telaPesquisarVeiculosAlug(void);
+void telaAtualizarVeiculosAlug(void);
+void telaExcluirVeiculosAlug(void);
+void cadastrarVeiculosAlug(void);
+void pesquisarVeiculosAlug(void);
+void atualizarVeiculosAlug(void);
+void excluirVeiculosAlug(void);
 
 void moduloRelatorios(void);
 
@@ -146,7 +155,6 @@ void moduloCliente(void) {
 void cadastrarCliente(void) {
     telaCadastrarCliente();
 }
-
 
 void pesquisarCliente(void) {
     telaPesquisarCliente();
@@ -344,7 +352,6 @@ void cadastrarAdministrador(void) {
     telaCadastrarAdministrador();
 }
 
-
 void pesquisarAdministrador(void) {
     telaPesquisarAdministrador();
 }
@@ -541,7 +548,6 @@ void cadastrarVeiculosDisp(void) {
     telaCadastrarVeiculosDisp();
 }
 
-
 void pesquisarVeiculosDisp(void) {
     telaPesquisarVeiculosDisp();
 }
@@ -711,10 +717,191 @@ void telaExcluirVeiculosDisp(void) {
 // Módulo Veículos Alugados //
 //////////////////////////////
 
-
+//Inspirado no modelo de @flgorgonio
 void moduloVeiculosAlug(void) {
-	// modulo em desenvolvimento
+    char escolha;
+    do {
+        escolha = telaMenuVeiculosAlug();
+        switch(escolha) {
+            case '1': cadastrarVeiculosAlug();
+                                break;
+            case '2': pesquisarVeiculosAlug();
+                                break;
+            case '3': atualizarVeiculosAlug();
+                                break;
+            case '4': excluirVeiculosAlug();
+                                break;
+        }
+    } while (escolha != '0');                                                      
+}
 
+
+void cadastrarVeiculosAlug(void) {
+    telaCadastrarVeiculosAlug();
+}
+
+void pesquisarVeiculosAlug(void) {
+    telaPesquisarVeiculosAlug();
+}
+
+void atualizarVeiculosAlug(void) {
+    telaAtualizarVeiculosAlug();
+}
+
+void excluirVeiculosAlug(void) {
+    telaExcluirVeiculosAlug();
+}
+
+
+char telaMenuVeiculosAlug(void) {
+    char op;
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = Menu Veículos Alugados  = = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           1. Cadastrar um novo veículo alugado                        ///\n");
+    printf("///           2. Pesquisar os dados de um veículo alugado                 ///\n");
+    printf("///           3. Atualizar o cadastro de um veículo alugado               ///\n");
+    printf("///           4. Excluir um veículo alugado do sistema                    ///\n");
+    printf("///           0. Voltar ao menu anterior                                  ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Escolha a opção desejada: ");                       
+    scanf("%c", &op);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    return op;
+}
+
+
+void telaCadastrarVeiculosAlug(void) {
+    char placa[8];
+    char nome[51];
+    char marca[51];
+    char ano[5];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = Cadastrar Veículos Alugados = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Placa (modelo Mercosul/letras maiúsculas): ");
+    scanf("%[A-Z0-9]", placa);
+    getchar();
+    printf("///           Nome: ");
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9]", nome);
+    getchar();
+    printf("///           Marca: ");
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ 0-9]", marca);
+    getchar();
+    printf("///           Ano: ");
+    scanf("%[0-9]", ano);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaPesquisarVeiculosAlug(void) {
+    char placa[8];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = Pesquisar Veículos Alugados = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe a placa (modelo Mercosul/letras maiúsculas): ");
+    scanf("%[A-Z0-9]", placa);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaAtualizarVeiculosAlug(void) {
+    char placa[8];
+
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = Atualizar Veículos Alugados = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe a placa (modelo Mercosul/letras maiúsculas): ");
+    scanf("%[A-Z0-9]", placa);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+
+void telaExcluirVeiculosAlug(void) {
+    char placa[8];
+    
+    system("clear||cls");
+    printf("\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///          = = = = = = Locadora de Veículos RM = = = = = =              ///\n");
+    printf("///                                                                       ///\n");
+    printf("///          Developed by  @rusdrael and @matheusfaria21 - Out, 2021      ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                       ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///           = = = = = = Excluir Veículos Alugados = = = = =             ///\n");
+    printf("///           = = = = = = = = = = = = = = = = = = = = = = = =             ///\n");
+    printf("///                                                                       ///\n");
+    printf("///           Informe a placa (modelo Mercosul/letras maiúsculas): ");
+    scanf("%[A-Z0-9]", placa);
+    getchar();
+    printf("///                                                                       ///\n");
+    printf("///                                                                       ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
 }
 
 
